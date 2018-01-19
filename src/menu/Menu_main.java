298efde8aacdbc05c1ae5deb93f304b7e9baeb0e
itoutakahiro <itoutakahiro@192.168.11.2>
2018-01-20 07:51:48 +0900

    kadai0115

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view view = new Menu_view();
+		view.setBounds(5, 5, 755, 475);
+		view.setVisible(true);
+
+	}
+
+}
